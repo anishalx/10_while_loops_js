15f824d80ffc36d447caf714224331013101fc62
Anish <113669810+radlysamar@users.noreply.github.com>
2022-10-03 05:21:29 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+//while loop
+// let n = prompt("Enter the number -")
+// n = Number.parseInt(n)
+// s =1;
+// while (s<=n){
+//   console.log(s)
+//   s++
+//   }
+
+
+// do while loop
+let n = prompt("Enter the number")
+n = Number.parseInt(n)
+s =1;
+do{
+  console.log(s)
+  s++
+  }
+while (s<=n)
